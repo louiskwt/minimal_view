14fbdffa236b1fbff5ecd3cc01b5b4a5f89f06c3
Louis Tsang <kawingt2@gmail.com>
2021-02-05 22:51:40 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const filteredList = [
+    "*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+    "*://*.adtechus.com/*",
+    "*://choices.trustarc.com/*",
+    "*://*.goo.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return {cancel: true}},
+    {urls: filteredList},
+    ["blocking"]
+)
